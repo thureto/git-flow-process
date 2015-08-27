e16d857647a3c450005374c35a45092c60384621
Olivier <sulfuric80@hotmail.com>
2015-08-27 22:22:08 +0200

    first commit

---
+++ app/src/androidTest/java/com/othuret/draft/git/gitflowprocess/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.othuret.draft.git.gitflowprocess;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
